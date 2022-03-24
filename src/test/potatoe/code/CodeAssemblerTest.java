157b45f3ad126bd6f8fcf23e82612be7a6137a58
Niklas Milbradt <milbradt@ponton.de>
2022-03-24 16:51:24 +0100

    prettier test base

--- src/test/potatoe/code/CodeAssemblerTest.java
+++ src/test/potatoe/code/CodeAssemblerTest.java
@@ -1,6 +1,7 @@
 package potatoe.code;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.File;
@@ -25,19 +26,30 @@ public class CodeAssemblerTest {
     }
 
     private void checkLineOutput(final byte[] actual, final byte[] expectedBytes) {
-        assertThat(actual.length, equalTo(expectedBytes.length));
+        assertThat(actual.length, is(expectedBytes.length));
         for (int i=0; i<actual.length; i++) {
-            assertThat(actual[i], equalTo(expectedBytes[i]));
+            assertThat(actual[i], is(expectedBytes[i]));
         }
     }
 
     @Test
     public void testAssembleMoveWithImm8() {
         final CodeAssemblerOutput output = classUnderTest.assembleLine("MOV areg,255");
-        assertThat(output.getErrors().isEmpty(), equalTo(true));
-        assertThat(output.getWarnings().isEmpty(), equalTo(true));
+        assertThat(output.getErrors().isEmpty(), is(true));
+        assertThat(output.getWarnings().isEmpty(), is(true));
 
         final byte[] expectedBytes = new byte[] { 0b00000000, (byte) 0b11111111 };
+        checkLineOutput(output.getAssembledBytes(), expectedBytes);
+    }
+
+    @Test
+    public void testAssembleMoveWithBiggerImm8() {
+        final CodeAssemblerOutput output = classUnderTest.assembleLine("MOV areg,256");
+        assertThat(output.getErrors().isEmpty(), is(true));
+        assertThat(output.getWarnings().isEmpty(), is(false));
+        assertThat(output.getWarnings().get(0).getMessage(), equalTo("Value bigger than one byte. Cropped to fit: 256 -> 0"));
+
+        final byte[] expectedBytes = new byte[] { 0b00000000, (byte) 0b00000000 };
         checkLineOutput(output.getAssembledBytes(), expectedBytes);
     }
 
